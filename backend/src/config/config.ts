ad123bfaeae0e68c5ac6f4613e595dcf5b1686e7
Oleg Nyunkov <nvo84@yandex.ru>
2023-03-21 23:13:50 +0600

    fix before deploy

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,18 +6,18 @@ import { Offer } from '../offers/entities/offers.entity';
 
 export const typeOrmConfig = (configService: ConfigService) => ({
   type: configService.get('DB_TYPE') || 'localhost',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT') || 5432,
-  username: configService.get('DB_USERNAME'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_DATABASE'),
-  schema: configService.get('DB_SCHEMA'),
+  host: configService.get('POSTGRES_HOST'),
+  port: configService.get('POSTGRES_PORT') || 5432,
+  username: configService.get('POSTGRES_USER'),
+  password: configService.get('POSTGRES_PASSWORD'),
+  database: configService.get('POSTGRES_DB'),
+  schema: configService.get('POSTGRES_SCHEMA'),
   entities: [User, Wish, Wishlist, Offer],
-  synchronize: configService.get<boolean>('DB_SYNCHRONIZE') || false,
+  synchronize: configService.get<boolean>('POSTGRES_POSTGRES_SYNCHRONIZE') || false,
 });
 
 export const jwtConfig = (configService: ConfigService) => ({
-  secret: configService.get('JWT_SECRET-KEY'),
+  secret: configService.get('JWT_SECRET'),
   signOptions: {
     expiresIn: configService.get('JWT_LIFETIME'),
   },

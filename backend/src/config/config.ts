678859cb3430877a26858a39d31cdef02025f0a9
Oleg Nyunkov <nvo84@yandex.ru>
2023-03-22 23:58:46 +0600

    back fix

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,14 +6,14 @@ import { Offer } from '../offers/entities/offers.entity';
 
 export const typeOrmConfig = (configService: ConfigService) => ({
   type: configService.get('DB_TYPE') || 'localhost',
-  host: configService.get('POSTGRES_HOST'),
-  port: configService.get('POSTGRES_PORT') || 5432,
-  username: configService.get('POSTGRES_USER'),
-  password: configService.get('POSTGRES_PASSWORD'),
-  database: configService.get('POSTGRES_DB'),
-  schema: configService.get('POSTGRES_SCHEMA'),
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT') || 5432,
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  // schema: configService.get('POSTGRES_SCHEMA'),
   entities: [User, Wish, Wishlist, Offer],
-  synchronize: configService.get<boolean>('POSTGRES_SYNCHRONIZE') || false,
+  synchronize: configService.get<boolean>('DB_SYNCHRONIZE') || false,
 });
 
 export const jwtConfig = (configService: ConfigService) => ({

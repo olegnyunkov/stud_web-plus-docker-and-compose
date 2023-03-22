259a3b483929952cb85c908c7101a8e3bdb3e18f
Oleg Nyunkov <nvo84@yandex.ru>
2023-03-22 22:59:52 +0600

    cors fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
